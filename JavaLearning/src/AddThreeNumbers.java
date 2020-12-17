3ef7bda82192c51ab1c7a0a6d229b6ad939d06e8
brgujjula3gs <bharath.gitxs@gmail.com>
2020-12-17 11:15:36 -0500

    New 3 added

---
+++ JavaLearning/src/AddThreeNumbers.java
@@ -0,0 +1,14 @@
+public class AddThreeNumbers {
+
+    public static void main(String args[]){
+
+        int a= 10;
+        int b= 20;
+        int c = 30;
+        int d;
+
+        d=a+b+c;
+
+        System.out.println(d);
+    }
+}
