7513063e4b62923207f7abc627560fe3bd6223a7
brgujjula3gs <bharath.gitxs@gmail.com>
2020-12-18 10:59:28 -0500

    a

---
+++ JavaLearning/src/MultiplyTwoNumbers.java
@@ -0,0 +1,13 @@
+public class MultiplyTwoNumbers {
+    public static void main(String args[]){
+
+        int a= 10;
+        int b= 20;
+        int c = 30;
+        int d;
+
+        d=a*b*c;
+
+        System.out.println(d);
+    }
+}
